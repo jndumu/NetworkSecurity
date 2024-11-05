a4d3d4f47d7a3886185b67571d627f2dfcefcc67
josie <latinue@gmail.com>
2024-11-05 16:20:28 -0500

    ETL Pipeline completed

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -12,8 +12,7 @@ class NetworkSecurityException(Exception):
     def __str__(self):
         return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
         self.file_name, self.lineno, str(self.error_message))
-
-"""       
+        
 if __name__=='__main__':
     try:
         logger.logging.info("Enter the try block")
@@ -21,4 +20,3 @@ if __name__=='__main__':
         print("This will not be printed",a)
     except Exception as e:
            raise NetworkSecurityException(e,sys)
-"""
